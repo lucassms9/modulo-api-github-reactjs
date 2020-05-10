40dcce03df3ed7dfc700a1ce7ff16f17e8c4ce89
lucas santos <lucas.santos@dedtechsolutions.com.br>
2020-05-10 00:54:47 -0300

    aula 9

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Routes from './routes';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <>
+        <Routes />
+        <GlobalStyle />
+      </>
     </div>
   );
 }
