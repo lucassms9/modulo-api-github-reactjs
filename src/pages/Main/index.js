292b5e29fb25d0f5b7f7f3c37568164b0e1b3d1e
lucas santos <lucas.santos@dedtechsolutions.com.br>
2020-05-10 13:56:49 -0300

    fim mod 5

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { FaGitAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 import api from '../../services/api';
-import { Container, Form, SubmitButton, List } from './styles';
+import { Form, SubmitButton, List } from './styles';
+import Container from '../../components/Container';
 
 export default class Main extends Component {
   constructor(props) {
@@ -78,7 +80,9 @@ export default class Main extends Component {
           {repositories.map((repo) => (
             <li key={repo.name}>
               <span>{repo.name}</span>
-              <a href="#">Detalhes</a>
+              <Link to={`/repository/${encodeURIComponent(repo.name)}`}>
+                Detalhes
+              </Link>
             </li>
           ))}
         </List>
--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,5 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import api from '../../services/api';
+import Container from '../../components/Container';
+import { Loading, Owner } from './styles';
 
-export default function Repository() {
-  return <h1>Repository</h1>;
+class Repository extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      repository: [],
+      issues: [],
+      loading: true,
+    };
+  }
+
+  async componentDidMount() {
+    const { match } = this.props;
+    const repoName = decodeURIComponent(match.params.repository);
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { repository, issues, loading } = this.state;
+    if (loading) {
+      return <Loading>Carregando</Loading>;
+    }
+
+    return (
+      <Container>
+        <Owner>
+          <Link to="/">Voltar aos repos</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+      </Container>
+    );
+  }
 }
+
+Repository.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      repository: PropTypes.string,
+    }),
+  }).isRequired,
+};
+
+export default Repository;
